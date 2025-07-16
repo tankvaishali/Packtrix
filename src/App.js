c4e099cce113817176747c965025c5a9535d5b33
tankvaishali <vaishalitank28603@gmail.com>
2025-07-16 16:19:48 +0530

    create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Component/HomePage/Home';
 
 function App() {
   return (
    <>
-   
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+   </Routes>
+   </BrowserRouter>
    </>
   );
 }
