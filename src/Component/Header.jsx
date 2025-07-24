f06f96a857370b28091c713fecfe0d93caf169e5
Mital-123 <mailto:mittalvaghela2003@gmail.com>
2025-07-24 17:28:40 +0530

    header changes

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from 'react-router-dom';
 function Header() {
     return (
         <div className="">
-            <Navbar expand="lg" className="bg-white px-3 px-lg-4 shadow gradient-border">
+            <Navbar expand="lg" className="bg-white px-3 px-lg-4 shadow">
                 <Navbar.Brand as={Link} to="/">
                     <div style={{ width: '170px', height: 'auto' }}>
                         <img
@@ -39,19 +39,19 @@ function Header() {
                         <Nav className="justify-content-end flex-grow-1 pe-3 gap-2">
                             <NavLink
                                 to="/"
-                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu rounded text-decoration-none"
+                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu text-decoration-none"
                             >
                                 Home
                             </NavLink>
                             <NavLink
                                 to="/about"
-                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu rounded text-decoration-none"
+                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu text-decoration-none"
                             >
                                 About Us
                             </NavLink>
                             <NavLink
                                 to="/blog"
-                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu rounded text-decoration-none"
+                                className="border text-center px-2 py-1 fw-bold text-white navbar_menu text-decoration-none"
                             >
                                 Blog
                             </NavLink>
